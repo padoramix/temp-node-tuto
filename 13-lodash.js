1c5f11d05f5a071edcb65cfca7c34bd1bddc6819
Gregory Devant <gdevant@banque-edel.fr>
2023-02-08 14:05:53 +0100

    Update from the course

---
+++ 13-lodash.js
@@ -0,0 +1,11 @@
+// Manual approach (creates package.json in the root, create properties, etc..)
+// npm init (step by step install)
+// npm init -y (everything default)
+
+const _ = require('lodash')
+
+const items = [1, [2, [3, [4, [ 5]]]]]
+const newItems = _.flattenDeep( items )
+
+console.log(newItems)
+console.log('Hello people')
